afc052b00b5bc1e1f78c08bbf2b622629c1b7b8c
Vitor Justamand <justamandd@gmail.com>
2022-11-21 20:27:33 -0300

    atualização do banco de dados

--- src/migrations/transactions.migration.js
+++ src/migrations/transactions.migration.js
@@ -9,13 +9,19 @@ try {
         place VARCHAR2(50),
         method VARCHAR2(15) NOT NULL,
         created_at TIMESTAMP DEFAULT localtimestamp,
-        fk_tickets_ticket_id CHAR(36)
+        fk_tickets_ticket_id CHAR(36),
+        fk_recharges_recharge_id INT
     );
     
     ALTER TABLE transactions ADD CONSTRAINT FK_transactions_2
         FOREIGN KEY (fk_tickets_ticket_id)
         REFERENCES tickets (ticket_id)
-        ON DELETE CASCADE
+        ON DELETE CASCADE;
+
+    ALTER TABLE transactions ADD CONSTRAINT FK_transactions_3
+        FOREIGN KEY (fk_recharges_recharge_id)
+        REFERENCES recharges (recharge_id)
+        ON DELETE RESTRICT
     `, [])
 } catch (error) {
     console.error(error.message)
