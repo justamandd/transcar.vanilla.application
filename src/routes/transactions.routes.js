edb8e17ffd5f5c676cd4182ee82460e9ab31a568
Vitor Justamand <justamandd@gmail.com>
2022-11-21 21:44:47 -0300

    selection by different ways added

--- src/routes/transactions.routes.js
+++ src/routes/transactions.routes.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const transactionsController = require('../controllers/transactions.controller');
 
 // router.get('/', transactionsController.list);
-// router.get('/', transactionsController.select);
+router.get('/recharges', transactionsController.selectByRecharges);
+router.get('/tickets', transactionsController.selectByTickets);
+router.get('/', transactionsController.select);
 router.post('/', transactionsController.doTransaction);
 
 module.exports = router;
